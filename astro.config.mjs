5963cf46fe7886e766312ed83ad524ae747d496b
marcusleonas <wishbone@posteo.uk>
2026-04-17 12:19:14 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
     format: "preserve",
   },
+  site: "https://n3bula.lol",
+  integrations: [sitemap()],
 });
